6a21e20fde731d7631425350ebb44d68d657ca9e
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2024-03-16 20:20:19 +0300

    111

--- src/main/java/net/sanberdir/legends_lost_worlds/client/ModCreativeTab.java
+++ src/main/java/net/sanberdir/legends_lost_worlds/client/ModCreativeTab.java
@@ -3,11 +3,13 @@ package net.sanberdir.legends_lost_worlds.client;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.sanberdir.legends_lost_worlds.common.items.InitItemsLLW;
+
 public class ModCreativeTab {
-    public static final CreativeModeTab LLW_CREATIVE = new CreativeModeTab("llw_creative") {
+    public static final CreativeModeTab LLW_CREATIVE = new CreativeModeTab("llw") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(Items.CRYING_OBSIDIAN);
+            return new ItemStack(InitItemsLLW.FIRST_BOOK.get());
         }
     };
 }
