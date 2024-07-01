83d5e4d6ed3ffabd22281d751c86efa90bc00ebe
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2024-07-01 16:36:41 +0300

    changr

--- src/main/java/net/sanberdir/llw/common/quest/QuestRender.java
+++ src/main/java/net/sanberdir/llw/common/quest/QuestRender.java
@@ -17,32 +17,31 @@ import org.lwjgl.opengl.GL11;
 
 
 public class QuestRender {
-    private final ResourceLocation QUEST_TYPE_TEXTURE = new ResourceLocation(LLW.MOD_ID, "textures/gui/quest_type_icons.png");
+
+    private static final ResourceLocation QUEST_TYPE_TEXTURE = new ResourceLocation(LLW.MOD_ID, "textures/gui/quest_type_icons.png");
 
     public QuestRender(Quest quest, GuiGraphics guiGraphics, int scissorX, int scissorY, int scissorWidth, int scissorHeight, int screenCenterX, int screenCenterY, int textureX, int textureY) {
         int positionX = screenCenterX + textureX + quest.getPosition().getX();
         int positionY = screenCenterY + textureY + quest.getPosition().getY();
 
-        renderQuestType(guiGraphics, quest, positionX, positionY, scissorX, scissorY, scissorWidth, scissorHeight);
-        renderIcon(quest, positionX, positionY, scissorX, scissorY, scissorWidth, scissorHeight, guiGraphics);
+        this.renderQuestType(guiGraphics, quest, positionX, positionY, scissorX, scissorY, scissorWidth, scissorHeight);
+        this.renderIcon(quest, positionX, positionY, scissorX, scissorY, scissorWidth, scissorHeight, guiGraphics);
     }
 
     private void renderQuestType(GuiGraphics guiGraphics, Quest quest, int positionX, int positionY, int scissorX, int scissorY, int scissorWidth, int scissorHeight) {
         RenderSystem.enableScissor(scissorX, scissorY, scissorWidth, scissorHeight);
-        // Очищаем область перед отрисовкой новой текстуры
         RenderSystem.clearColor(0.0F, 0.0F, 0.0F, 0.0F);
         RenderSystem.clear(GL11.GL_DEPTH_BUFFER_BIT, Minecraft.ON_OSX);
 
         if (quest.isActive()) {
-            guiGraphics.blit(QUEST_TYPE_TEXTURE, positionX - 2, positionY - 1, 1, 33, 22, 54, 86, 54);
+            guiGraphics.blit(QUEST_TYPE_TEXTURE, positionX - 2, positionY - 1, 0, 32, 22, 22, 86, 54);
         } else {
-            guiGraphics.blit(QUEST_TYPE_TEXTURE, positionX - 2, positionY - 1, 1, 1, 22, 22, 86, 54);
+            guiGraphics.blit(QUEST_TYPE_TEXTURE, positionX - 2, positionY - 1, 0, 0, 22, 22, 86, 54);
         }
         RenderSystem.disableScissor();
     }
 
     private void renderIcon(Quest quest, int x, int y, int scissorX, int scissorY, int scissorWidth, int scissorHeight, GuiGraphics guiGraphics) {
-        ItemRenderer renderItem = Minecraft.getInstance().getItemRenderer();
         String[] id = quest.getTexture().split(":");
         ResourceLocation itemId = new ResourceLocation(id[0], id[1]);
 
@@ -57,4 +56,5 @@ public class QuestRender {
         guiGraphics.renderItem(itemStack, x, y);
         RenderSystem.disableScissor();
     }
+
 }
